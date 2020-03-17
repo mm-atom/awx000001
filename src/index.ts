a39cd8e0a325f5609f12b48d8e5e97d03035c35b
taoqf <tao_qiufeng@126.com>
2020-03-17 16:19:31 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-export default function fun() {
+/**
+ * 调用接口获取登录凭证（code）
+ * @see [微信官方文档](https://developers.weixin.qq.com/miniprogram/dev/api/wx.login.html)
+ */
+export default function get_code() {
+	return new Promise<string>((resolve, reject) => {
+		wx.login({
+			success(res) {
+				resolve(res.code);
+			},
+			fail(err) {
+				reject(new Error(err));
+			}
+		});
+	});
 }
